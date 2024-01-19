516bb0c0cae5f9fe35d7702148fca46a15049f04
Íñigo Aréjula Aísa <arejula27@gmail.com>
2024-01-19 16:13:26 +0100

    Add .flake8 configuration file and exercise_1.1.py with task descriptions

---
+++ exercise_1.1.py
@@ -0,0 +1,8 @@
+# Task 1.1 Calculate the Clock Granularity of different Python Timers (on your system).
+
+# Task 1.2 Timing the Julia set code functions
+
+# Task 1.3 T Profile the Julia set code with cProfile and line_profiler the computation
+
+# Task 1.4 Memory-profile the Juliaset code. Use the memory_profiler and mprof
+# to profile the computation in JuliaSet code.
