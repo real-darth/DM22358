c40ad4e539d1443ac7c2d7adc02a6ccc58044d49
Íñigo Aréjula Aísa <arejula27@gmail.com>
2024-01-20 10:52:34 +0100

    Add .gitignore entry for *.stat file and implement JuliaSet.py

--- exercise_1.1.py
+++ exercise_1.1.py
@@ -1,8 +1,24 @@
+import JuliaSet  # noqa: F401
+import cProfile
+import snakeviz
+
 # Task 1.1 Calculate the Clock Granularity of different Python Timers (on your system).
 
 # Task 1.2 Timing the Julia set code functions
 
 # Task 1.3 T Profile the Julia set code with cProfile and line_profiler the computation
 
+
+def task_1_3():
+    cProfile.run("JuliaSet.calc_pure_python(10000, 300)", "calc_pure_python.stat")
+    # for mor info see: https://jiffyclub.github.io/snakeviz/
+    snakeviz.view("calc_pure_python.stat")
+
+
 # Task 1.4 Memory-profile the Juliaset code. Use the memory_profiler and mprof
 # to profile the computation in JuliaSet code.
+
+
+# main
+if __name__ == "__main__":
+    task_1_3()
