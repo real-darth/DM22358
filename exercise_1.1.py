2e76328af3ff023ba1a66bd0da8ac8db15ebb82c
Elliot <18326767+real-darth@users.noreply.github.com>
2024-01-22 21:08:32 +0100

    updated exercise file

--- exercise_1.1.py
+++ exercise_1.1.py
@@ -1,7 +1,35 @@
 import JuliaSet  # noqa: F401
-
+import numpy as np
+import time
+from timeit import default_timer as timer
 
 # Task 1.1 Calculate the Clock Granularity of different Python Timers (on your system).
+# code for calculating the 'clock granularity', source from Canvas
+def checktick(time_func):
+   M = 200
+   timesfound = np.empty((M,))
+   for i in range(M):
+      t1 = time_func() # get timestamp from timer
+      t2 = time_func() # get timestamp from timer
+      while (t2 - t1) < 1e-16: # if zero then we are below clock granularity, retake timing
+          t2 = time_func() # get timestamp from timer
+      t1 = t2 # this is outside the loop
+      timesfound[i] = t1 # record the time stamp
+   minDelta = 1000000
+   Delta = np.diff(timesfound) # it should be cast to int only when needed
+   minDelta = Delta.min()
+
+   return minDelta
+
+# run the different time modules
+case_1 = checktick(time.time)
+case_2 = checktick(timer)
+case_3 = checktick(time.time_ns)
+
+# print the results of the different timers
+print("1 - time.time(): {} (s)".format(case_1))
+print("2 - timeit: {} (s)".format(case_2))
+print("3 - time.time_ns(): {} (ns)".format(case_3))
 
 # Task 1.2 Timing the Julia set code functions
 
