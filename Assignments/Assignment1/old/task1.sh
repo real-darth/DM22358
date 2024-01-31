738aab63a1acebfe8e00a93748adec1e11d9851f
ogronman <90456429+OsGrGreen@users.noreply.github.com>
2024-01-31 14:35:26 +0100

    Remove shell scripts

--- Assignments/Assignment1/old/task1.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "This file is deprecated..."
-echo "see Task1/task1.py for description on how to run the tasks"
-#python3.9 task1/JuliaSet_decorator.py
-#python3.9 -m cProfile -o task1/calc_pure_python.stat task1/JuliaSet.py
-#python3.9 lineProfCode.py task1/calc_pure_python.stat
-#python3.9 -m kernprof -l task1/JuliaSet_profile.py
-#python3.9 -m line_profiler task1/JuliaSet_profile.py.lprof
