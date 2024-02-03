7b3d1a6d265e7ffce86801a3752582def41c5a5b
OsGrGreen <ozkargronman@gmail.com>
2024-02-03 12:23:23 +0100

    Kinda finish task1?

--- Assignment2/src/task1/task1.py
+++ Assignment2/src/task1/task1.py
@@ -1,52 +1,97 @@
 from timeit import default_timer as timer
 import sys
+from array import array
+import matplotlib.pyplot as plt
 
 STREAM_ARRAY_SIZE = 10_000
 
-def run_stream_test():
-    a = [1.0]*STREAM_ARRAY_SIZE
-    b = [2.0]*STREAM_ARRAY_SIZE
-    c = [0.0]*STREAM_ARRAY_SIZE
+scalar = 2.0
 
-    scalar = 2.0
-    times = [0.0]*4
-    #copy
-    times[0] = timer()
-    for j in range(STREAM_ARRAY_SIZE):
+def get_copy_size(size):
+    return 2 * sys.getsizeof(float) * size
+def get_add_size(size):
+    return 2 * sys.getsizeof(float) * size
+def get_scale_size(size):
+    return 3 * sys.getsizeof(float) * size
+def get_triad_size(size):
+    return 3 * sys.getsizeof(float) * size
+
+def time_copy(a, c, size):
+    time = timer()
+    for j in range(size):
         c[j] = a[j]
-    times[0] = timer() - times[0]
+    return timer() - time
+
+def time_scale(b, c, size):
+    time = timer()
+    for j in range(size):
+        b[j] = scalar*c[j]
+    return timer() - time
+
+def time_sum(a, b, c, size):
+    time = timer()
+    for j in range(size):
+        c[j] = a[j]+b[j]
+    return timer() - time
+
+def time_triad(a, b, c, size):
+    time = timer()
+    for j in range(size):
+        a[j] = b[j]+scalar*c[j]
+    return timer() - time
+
+
+def run_stream_test(type, size,debug):
+    if type=="l":
+        a = [1.0]*size
+        b = [2.0]*size
+        c = [0.0]*size
+    else:
+        a = array('f', [1.0] * size)
+        b = array('f', [2.0] * size)
+        c = array('f', [0.0] * size)
+
+    times = [0.0]*4
+
+    #copy
+    times[0] = time_copy(a, c, size)
 
     # scale
-    times[1] = timer()
-    for j in range(STREAM_ARRAY_SIZE):
-        b[j] = scalar*c[j]
-    times[1] = timer() - times[1]
+    times[1] = time_scale(b, c, size)
      
      #sum
-    times[2] = timer()
-    for j in range(STREAM_ARRAY_SIZE):
-        c[j] = a[j]+b[j]
-    times[2] = timer() - times[2]
+    times[2] = time_sum(a, b, c, size)
 
     # triad
-    times[3] = timer()
-    for j in range(STREAM_ARRAY_SIZE):
-        a[j] = b[j]+scalar*c[j]
-    times[3] = timer() - times[3]
+    times[3] = time_triad(a, b, c, size)
 
-    copy = 2 * sys.getsizeof(float) * STREAM_ARRAY_SIZE
-    add = 2 * sys.getsizeof(float) * STREAM_ARRAY_SIZE
-    scale = 3 * sys.getsizeof([]) * STREAM_ARRAY_SIZE
-    triad = 3 * sys.getsizeof([]) * STREAM_ARRAY_SIZE
-    print("Copy size:", copy)
-    print("Add size:",add)
-    print("Scale size:",scale)
-    print("Triad size:",triad)
+    #print("Copy size:", copy)
+    #print("Add size:",add)
+    #print("Scale size:",scale)
+    #print("Triad size:",triad)
 
-    print("Copy size:",copy/times[0])
-    print("Copy size:",add/times[1])
-    print("Copy size:",scale/times[2])
-    print("Copy size:",triad/times[3])
+    copyStream = (get_copy_size(size)/times[0])/1e9
+    addStream = (get_add_size(size)/times[1])/1e9
+    scaleStream = (get_scale_size(size)/times[2])/1e9
+    triadStream = (get_triad_size(size)/times[3])/1e9
+
+    total = copyStream + addStream + scaleStream + triadStream
+    if debug:
+        print("Copy GB/s:",copyStream)
+        print("Add GB/s:",addStream)
+        print("Scale GB/s:",scaleStream)
+        print("Triad GB/s:",triadStream)
+
+    return total
+
 
 if __name__ == "__main__":
-    run_stream_test()
+    x = [i * 10 + 100 for i in range(100000) if i * 10 + 100 <= 100_000]
+    y1 = []
+    y2 = []
+    for val in x:
+        y1.append(run_stream_test("l",val, False))
+        y2.append(run_stream_test("array",val, False))
+    plt.plot(x,y1)
+    plt.plot(x,y2)
+    plt.show()
