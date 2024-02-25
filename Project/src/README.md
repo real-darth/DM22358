6f7f36a09d1a9a918603f40733181b3041765bfb
Elliot <elliot.darth@gmail.com>
2024-02-25 19:26:26 +0100

    updated cython files

---
+++ Project/src/README.md
@@ -0,0 +1,6 @@
+# Cython
+
+To check your cython code, and what can be improved, run:
+```
+cython -a tester.pyx
+```
--- Project/src/tester.pyx
+++ Project/src/tester.pyx
@@ -1,22 +1,25 @@
-import numpy as np
 cimport numpy as cnp
 from libc.math cimport cos, sin, atan2
 from cython.parallel import prange
 cimport cython
 
 @cython.boundscheck(False)  # Deactivate bounds checking
-@cython.wraparound(False)   # Deactivate negative indexing.
-cpdef void calculate_mean_theta(double[::1] x, double[::1] y, double[::1] theta, int N, double R, double[::1] mean_theta):
+cdef void _calculate_mean_theta(double[::1] x, double[::1] y, double[::1] theta, int N, double R, double[::1] mean_theta) nogil:
     cdef int b, i
     cdef double R_squared = R**2
     cdef double sx, sy, dist_squared
     
-    for b in prange(N, nogil=True):
+    for b in range(N):
         sx = 0
         sy = 0
-        for i in prange(b+1, N, nogil=False):
+        for i in prange(b + 1, N):
             dist_squared = (x[i] - x[b]) ** 2 + (y[i] - y[b]) ** 2
             if dist_squared < R_squared:
                 sx += cos(theta[i])
                 sy += sin(theta[i])
         mean_theta[b] = atan2(sy, sx)
+
+@cython.boundscheck(False)  # Deactivate bounds checking
+@cython.wraparound(False)   # Deactivate negative indexing.
+cpdef void calculate_mean_theta(cnp.ndarray[double, ndim=1] x, cnp.ndarray[double, ndim=1] y, cnp.ndarray[double, ndim=1] theta, int N, double R, cnp.ndarray[double, ndim=1] mean_theta):
+    _calculate_mean_theta(x, y, theta, N, R, mean_theta)
